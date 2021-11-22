c3b203329e442fe418c4d6180576346fdd0c3786
leothehero <leothehero@users.noreply.github.com>
2021-11-22 23:48:32 +0100

    Update GUSsUI.lua

--- GERTi/Modules/GUSsUI.lua
+++ GERTi/Modules/GUSsUI.lua
@@ -1,4 +1,4 @@
--- GUS Server UI - Beta 1 
+-- GUS Server UI - Beta 1
 print("Initiating GERT Update Suite User Interface - Server Edition")
 print()
 print("Preparing Modules...")
@@ -14,10 +14,14 @@ print("Loading hardware interface...")
 local computer = require("computer")
 print("Loading thread API...")
 local thread = require("thread")
+print("Loading event API...")
+local event = require("event")
+print("Loading GERT Update Server API...")
+local GUS = require("GERTUpdateServer")
 
 print("Preparing Internal Variables...")
 local gpu = component.gpu
-local screenWidth,screenHeight = gpu.getResolution()
+local oldResolution = {gpu.getResolution()}
 local PrimaryColour = 0xfc9414
 
 
@@ -50,32 +54,65 @@ local function GetCenterOffsets (textLen,screenWidth)
     return StartPoint
 end
 
-local function DrawMainScreen(Title,SubTitle)
-    term.clear()
-    AestheticSleep(0.2)    
-    local TopWidth = math.max(string.len(Title),string.len(SubTitle))
-    local startPoint = GetCenterOffsets(TopWidth,screenWidth)
-    term.setCursor(startPoint,1)
-    term.write("╔"..string.rep("═",TopWidth).."╗")
-    local joinString = "╔" .. string.rep("═",(screenWidth-TopWidth)/2-4) .. "╩" .. string.rep("═",TopWidth) .. "╩" .. string.rep("═",(screenWidth-TopWidth)/2-4)
-    local i = 2
-    while i < screenHeight do
-        AestheticSleep(0.05)
-        if i == 3 then
-            term.setCursor(1,3)
-            term.write("╠"..string.rep("═",screenWidth-2).."╣")
-        else
-            gpu.set(1,i,"║")
-            gpu.set(screenWidth,i,"║")
+local function DrawMenuResize(Title,Options)
+    local Width = string.len(Title) + 2
+    local Height = 3
+    for k,v in pairs(Options) do
+        if type(v) ~= "function" then
+            k = "$"..k
         end
+        Width = math.max(Width,string.len(k)+4)
+        Height = Height + 1
+    end
+    gpu.setResolution(Width,Height)
+    term.clear()
+    term.write(" " ..Title)
+    term.setCursor(1,2)
+    term.write("╔"..string.rep("═",Width-2).."╗")
+    local i = 3
+    for k,v in pairs(Options) do
+        if type(v) ~= "function" then
+            k = "$"..k
+        end
+        gpu.set(1,i,"║")
+        gpu.set(Width,i,"║")
+        gpu.set(3,i,k)
         i = i + 1
     end
-    AestheticSleep(0.05)
-    term.setCursor(1,screenHeight)
-    term.write("╚"..string.rep("═",screenWidth-2).."╝")
-    AestheticSleep(0.05)
-    term.setCursor()
+    term.setCursor(1,Height)
+    term.write("╚"..string.rep("═",Width-2).."╝")
+    return Width, Height
 end
+
+local function MenuSelect(Width,Height,Options)
+    local cursor = 1
+    local WidthTable = {}
+    for k,v in pairs(Options) do
+        if type(v) ~= "function" then
+            k = "$"..k
+        end
+        WidthTable[cursor] = string.len(k)
+        cursor = cursor + 1
+    end
+    cursor = 1
+    local selected = false
+    gpu.set(2,3,">")
+    gpu.set(2+WidthTable[cursor]+1,3,"<")
+    --[[repeat
+        gpu.set(2,cursor+2," ")
+        gpu.set(2,cursor+WidthTable[cursor]+2," ")
+    until selected]]
+    
+end
+
+
+
+
+
+
+
+
+
 
 
 
@@ -87,5 +124,9 @@ term.clear()
 AestheticSleep(0.5)
 
 
+local Width, Height = DrawMenuResize("Main Menu",GUS)
+local result = MenuSelect(Width, Height, GUS)
+os.sleep(1)
+event.pull()
+gpu.setResolution(oldResolution[1],oldResolution[2])
 
-DrawMainScreen("GERT Update Server","Main Menu")
